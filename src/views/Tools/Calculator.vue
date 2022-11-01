dcf428cb68a0fedc5909eccb4fb5b084a7ce2d6f
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2022-11-01 09:27:18 -0500

    update

--- src/views/Tools/Calculator.vue
+++ src/views/Tools/Calculator.vue
@@ -71,10 +71,13 @@ export default {
 </script>
 <style>
 .btn-alternative {
-    @apply py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover: bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
+    @apply py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border 
+    border-gray-200 hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 
+    dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
 }
 
 .btn-default {
-    @apply text-white bg-blue-700 hover: bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
+    @apply text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg 
+    text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
 }
 </style>
